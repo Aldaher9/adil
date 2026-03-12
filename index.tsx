35084ff836e16c9966df502310fea02cb650ea43
Aldaher9 <166499726+Aldaher9@users.noreply.github.com>
2026-03-13 03:45:38 +0400

    Delete index.tsx

--- index.tsx
+++
@@ -1,5 +0,0 @@
-// Angular bootstrap disabled to allow Vanilla JS application in index.html to run without conflicts.
-// The user has chosen to use a standalone index.html implementation.
-console.log('Running in Vanilla JS mode.');
-
-// AI Studio always uses an `index.tsx` file for all project types.
